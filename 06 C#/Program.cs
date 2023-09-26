21726a052cfdff005ce4c367a3c799c52b3bef89
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2023-09-26 11:04:53 +0100

    C# example created

---
+++ 06 C#/Program.cs
@@ -0,0 +1,16 @@
+using System; 
+
+namespace ConsoleApp
+{
+        /// <summary>
+    /// The main method in this class is called first
+    /// when the application is started.
+    /// 
+    /// This template C# project file authored by Nick Day
+    /// </summary>
+    public class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("Hello World");
+        }
+    }
+}
