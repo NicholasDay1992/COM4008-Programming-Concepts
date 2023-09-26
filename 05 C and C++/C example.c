007d00cb35999744b98c05599f40850a011dcea7
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2023-09-26 10:58:42 +0100

    working C and C++ example

---
+++ 05 C and C++/C example.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int number = 5;
+    if (number > 0){
+        printf("The number is positive.\n");
+    }    
+    return 0;
+}
---
+++ 05 C and C++/C++ example.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int numbers[5] = {1,2,3,4,5};
+    for (int i = 0; i < 5; i++){
+        std::cout << numbers[i] << std::endl;
+    }    
+    return 0;
+}
