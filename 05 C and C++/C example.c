4b2cf0494368facb560921562d6a9945012e6549
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2023-09-29 11:01:56 +0100

    added exercise outlines

--- 05 C and C++/C example.c
+++ 05 C and C++/C example.c
@@ -1,9 +1,74 @@
 #include <stdio.h>
 
+// README: 
+// Skip down to the exercise functions that are outlined below. 
+// Above each exercise you'll see a set of instructions. 
+
+/* Here are the definitions for each function */
+
+void exercise1();
+void exercise2();
+void exercise3();
+void exercise4();
+void exercise5();
+
+/* This is the main function - where programs in C start */
+/* For the purpose of this file, we'll use it to run your exercise attempts */
+
 int main() {
-    int number = 5;
-    if (number > 0){
-        printf("The number is positive.\n");
-    }    
-    return 0;
+
+    printf("Hello, World!\n");
+    printf("Welcome to your exercises in C!\n\n");
+
+    exercise1();
+    //call exercise2() here...
+    //call exercise3() here...
+    //call exercise4() here...
+    //call exericse5() here...
+
+    return 0; 
+
 }
+
+// Exercise 1 
+// 
+
+void exercise1(){
+
+    printf("Hello from the exercise 1 function...\n");
+    // Write the solution to exercise 1 here...
+} 
+
+// Exercise 2 
+// 
+
+void exercise2(){
+
+    // Write the solution to exercise 2 here...
+} 
+
+// Exercise 3 
+// 
+
+void exercise3(){
+
+    // Write the solution to exercise 3 here...
+} 
+
+// Exercise 4
+// 
+
+
+void exercise4(){
+
+    // Write the solution to exercise 4 here...
+} 
+
+// Exercise 5
+// 
+
+void exercise5(){
+
+    // Write the solution to exercise 5 here...
+} 
+
