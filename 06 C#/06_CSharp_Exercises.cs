0c0eb980c747d90327393bc67eaacbea82043ae0
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2023-09-29 15:49:12 +0100

    updated names to include exercises

--- 06 C#/06_CSharp_Exercises.cs
+++ 06 C#/06_CSharp_Exercises.cs
@@ -1,6 +1,6 @@
 using System; 
 
-namespace ConsoleApp
+namespace 06_CSharp
 {
         /// <summary>
     /// The main method in this class is called first
@@ -8,7 +8,7 @@ namespace ConsoleApp
     /// 
     /// This template C# project file authored by Nick Day
     /// </summary>
-    public class Program{
+    public class 06_CSharp{
         public static void Main(string[] args){
             Console.WriteLine("Hello World");
         }
