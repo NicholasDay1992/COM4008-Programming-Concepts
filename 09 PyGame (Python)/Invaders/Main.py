ef16f9ec5bc68eeb8c7175a5d351fd678f36bff7
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2023-10-15 09:51:03 +0100

    2d table of invaders

--- 09 PyGame (Python)/Invaders/Main.py
+++ 09 PyGame (Python)/Invaders/Main.py
@@ -19,19 +19,35 @@ SCREEN_X = 500
 SCREEN_Y = 700
 screen = pygame.display.set_mode([SCREEN_X, SCREEN_Y])
 
-# instantiate Player class
+# 1.0 instantiate Player class
 player = Player((SCREEN_X/2)-(35/2), (SCREEN_Y - 100), pygame.image.load("img/defender.png"), 35, 30)
 
-# instantiate Invader class
+# 1.1 instantiate Invader class
 invader = Invader(50, 100, pygame.image.load("img/invader1.png"), 30, 30 )
 
 invaders_x_left = 50
 invaders_y_top = 100
 
-#invaders = [0,0,0,0,0,0,0,0,0,0,0,0]
-invaders = [0] * 11 #short hand way to initialise all 11 mem spaces
-for i in range(0,11): # 1 to 11 - create one row
+#1.2 invaders = [0,0,0,0,0,0,0,0,0,0,0,0]
+invaders = [0] * 11 #short hand way to initialise all 11 mem spaces for ONE ROW
+for i in range(0,11): # 0 to 10 - create one row of 11 cols
     invaders[i] = Invader(invaders_x_left + (i * 40), 100, pygame.image.load("img/invader1.png"), 30, 30) #instantiate one row 
+
+#1.3 2D table of invaders 
+rows = 5
+cols = 11
+           # [[0] * col for _ in range(row)]
+invaders2D = [[0] * cols for i in range(rows)] #2D array using the Python list comprehension
+
+current_row = invaders_y_top
+#print(invaders2D)
+for i in range(rows): # 5 rows
+    for j in range(cols): # 0 to 10 - create one row of 11 cols
+        #invaders2D[i][j] = ((invaders_x_left + (j * 30) + 20), current_row )
+        invaders2D[i][j] = Invader((invaders_x_left + (j * 30) + 20), current_row, pygame.image.load("img/invader1.png"), 30, 30) #instantiate one row 
+    current_row = invaders_y_top + (50 * (i+1)) # distance between rows
+
+#print(invaders2D)
 
 # Fill the background with black - as it is Space Invaders
 screen.fill((0, 0, 0))
@@ -46,17 +62,24 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #1. draw the png on the screen!
+    #1.0 draw the png on the screen!
     #screen.blit(player.img, (player.x, player.y))
     
+    #1.1 draw one of the invaders
     #screen.blit(invader.img, (invader.x, invader.y))
     
-    i = 0
-    for x in invaders:
-        screen.blit(invaders[i].img, (invaders[i].x, invaders[i].y))
-        i += 1
+    #1.2 draw a row of invaders
+    #i = 0
+    #for x in invaders:
+    #    screen.blit(invaders[i].img, (invaders[i].x, invaders[i].y))
+    #    i += 1
     
-    # detect keypress
+    #1.3 draw 2d table of invaders
+    for i in range(rows):
+        for j in range(cols):
+            screen.blit(invaders2D[i][j].img, (invaders2D[i][j].x, invaders2D[i][j].y))
+
+    #1.0 detect keypress
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
             #screen.blit(player.img, (player.x, player.y)) 
