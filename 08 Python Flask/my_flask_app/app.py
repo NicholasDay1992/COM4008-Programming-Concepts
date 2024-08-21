acb109bfd719120d1c4234f3b502d7f1540e5f34
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2024-08-21 17:51:05 +0100

    added templates and html

--- 08 Python Flask/my_flask_app/app.py
+++ 08 Python Flask/my_flask_app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 # First Flask App
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html')
+
+'''    
+@app.route('/about')
+def about():
+    return "This is the About page."
+''' 
 
 if __name__ == '__main__':
     app.run(debug=True)
