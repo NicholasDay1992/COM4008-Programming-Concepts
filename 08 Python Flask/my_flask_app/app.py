bc8fd6b89380138cd2009625a612745bdb980c96
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2024-08-22 09:39:12 +0100

    updated first flask app to use CSS and also Python script

--- 08 Python Flask/my_flask_app/app.py
+++ 08 Python Flask/my_flask_app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from data_processing import process_data  # Import your script
 # First Flask App
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    result = process_data()  # Call the function from the script
+    return render_template('home.html', result=result)
  
 @app.route('/about')
 def about():
---
+++ 08 Python Flask/my_flask_app/data_processing.py
@@ -0,0 +1,6 @@
+
+def process_data():
+    # Simulate some data processing
+    l = [1,2,3,4,5,6,7]
+    total = sum(l)
+    return total
