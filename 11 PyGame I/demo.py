bd4db8b7839c064ba952b6c50ba1cda5b62c5a98
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2025-11-11 10:10:32 +0000

    update 11

--- 11 PyGame I/demo.py
+++ 11 PyGame I/demo.py
@@ -1,21 +1,45 @@
 #%reset -f
 import pygame 
+import sys
 pygame.init()
 
 screen = pygame.display.set_mode([500,500])
+
+player_x = 250
+player_img = pygame.image.load("11 PyGame I/defender.png") #load in the image
+
+player_img = pygame.transform.scale(player_img, (35, 30)) # change the scale
 
 running = True
 while running: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            pygame.display.quit()
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("Left arrow key pressed")
+                player_x -= 5
+            elif event.key == pygame.K_RIGHT:
+                print("Left arrow key pressed")
+                player_x += 5
+            elif event.key == pygame.K_ESCAPE or event.key == pygame.WINDOWCLOSE: # TO QUIT
+                running = False
+                #pygame.display.quit()
+                #pygame.QUIT()
+                #sys.exit()
+    
     screen.fill([0,0,0]) # black background
     
-    pygame.draw.circle(screen,(0,255,0), (250,250), 75) ## (0, 255, 0) = green
+    #pygame.draw.circle(screen,(0,255,0), [player_x, 250], 75) ## (0, 255, 0) = green
+    
+    screen.blit(player_img, (player_x, 450))
     
     pygame.display.flip()
-
-
-pygame.quit()
-pygame.display.quit()
-#del screen  
+    
+print("in loop: ",running)
+#pygame.display.quit()
+#pygame.quit()]
+sys.exit(0)
